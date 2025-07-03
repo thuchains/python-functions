5a560eb0160ed83cc6d9967d7a5fc3e301c9c5eb
Thu Vo <Thu.n.m.vo@gmail.com>
2025-07-03 13:24:57 +0700

    completed question 2

--- question2.py
+++ question2.py
@@ -1,21 +1,65 @@
-def simple_calculator(a, b)
-    add = a + b
-    subtract = a - b
-    multiply = a * b
-    divide = a / b
+def add(num1: float, num2: float) -> float:
+    result = num1 + num2
+    return result
 
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
-    print("Choose operation: \n1. Add \n2. Subtract \n3. Multiply \n4. Divide")
-    operation = float(input("Enter choice (1-4): "))
+def subtract(num1: float, num2: float) -> float:
+    result = num1 - num2
+    return result
 
-    if operation == 1:
-        result = 
-    elif operation == 2:
-        subtract
-    elif operation == 3:
-        multiply
+def multiply(num1: float, num2: float) -> float:
+    result = num1 * num2
+    return result
+
+def divide(num1: float, num2: float) -> float:
+    if num2 == 0:
+        print("Error: Cannot divide by zero")
     else: 
-        if b == 0:
-            print("Error, cannot divide by zero")
+        result = num1 / num2
+    return result
+        
+
+def simple_calculator(num1: float, num2: float, choice: int):
+    if choice == 1:
+        result = add(num1, num2)
+        print(f"{num1} + {num2} = {result}")
+    elif choice == 2:
+        result = subtract(num1, num2)
+        print(f"{num1} - {num2} = {result}")
+    elif choice == 3:
+        result = multiply(num1, num2)
+        print(f"{num1} * {num2} = {result}")
+    elif choice == 4: 
+        result = divide(num1, num2)
+        print(f"{num1} / {num2} = {result}")
+
+print("Simple Calculator")
+print("=================")
+
+while True:
+    print("\nCalculator Menu: ")
+    print("1. Add \n2. Subtract \n3. Multiply \n4. Divide \n5. Exit")
+    choice = input("Choose an opperation: ")
+
+    try:
+        choice = int(choice)
+    except ValueError:
+        print("Please enter a valid choice")
+        continue
+
+    if choice > 5 or choice < 1:
+        print("Please enter a value 1-5")
+        continue
+
+    if choice == 5:
+        print("Thank you for using the calculator!")
         break
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
+
+    simple_calculator(num1, num2, choice)
+        
